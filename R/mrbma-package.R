f1853311c41a627523be52c95701a5191e10d0f8
Michael Levin <mglev1n@gmail.com>
2022-11-30 22:16:19 -0500

    Added options to remove outliers and influential variants. Still need tests + addition of Nyholt procedure.

--- R/mrbma-package.R
+++ R/mrbma-package.R
@@ -3,9 +3,13 @@
 
 ## usethis namespace: start
 #' @importFrom tibble tibble
+#' @importFrom methods new
+#' @importFrom stats p.adjust qf
+#' @importFrom utils write.csv
 #' @import combinat
 #' @import hash
 #' @import TwoSampleMR
 #' @import dplyr
+#' @import ggplot2
 ## usethis namespace: end
 NULL
